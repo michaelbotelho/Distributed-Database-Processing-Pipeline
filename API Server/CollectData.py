6d61031955ac4f47a240acc16b1ef10af8b8f5cc
Michael Botelho <michaelbotelho51@gmail.com>
2024-04-08 20:33:08 -0400

    Revert "Check point, figured out context error"
    This reverts commit cb1e409658299d180f256ce58538438aa625038f.

--- API Server/CollectData.py
+++ API Server/CollectData.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 
 
-api = Flask(__name__)
+app = Flask(__name__)
 
 
 def find_open_port(address, start_port, end_port):
@@ -29,42 +29,11 @@ def find_open_port(address, start_port, end_port):
     return None
 
 
-def scrape_sports_events(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    
-    # Extract and Parse the HTML content of the page if request successful
-    events_list = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # Iterate through each row in table (event represented by a row)
-        event_items = soup.find_all('tr')
-        for event_item in event_items:
-            # Ensure table row is an event (otherwise operations will return none types)
-            if (event_item.get('itemtype') == 'http://schema.org/Event'):                
-                event_name = event_item.find('a', class_='text-primary')
-                sport = event_item.find('a', {'href': lambda x : x.startswith('/Sports/')})
-                country = event_item.find('meta', itemprop='addressCountry')
-                locality = event_item.find('meta', itemprop='addressLocality')
-                date = event_item.find_all('td')[-1]
-                
-                # Create JSON objects with scraped data and set default values in case attribute cant be found
-                events_list.append({
-                    'event_name': event_name.get_text() if event_name else None,
-                    'country': country.get('content') if country else None,
-                    'locality': locality.get('content') if locality else None,
-                    'date': date.get_text().strip() if date else None,
-                    'sport': sport.get_text() if sport else None
-                })
-        yield json.dumps(events_list).encode('utf-8')
-    else:
-        yield json.dumps(None).encode('utf-8')
-        
-        
 ''' Routing functions ''' 
-''' GET /Events
-@api.route("/events", methods=['POST'])
+# GET /Events
+@app.route("/")
 def process_request():
+    
     # Simulate a url query
     #with app.app_context():    
     with app.test_request_context('/?weeks=2'):
@@ -77,8 +46,40 @@ def process_request():
                 weeks = int(weeks)
         else:
             weeks = 0
+        
 
-            
+    def scrape_sports_events(url):
+        # Send a GET request to the URL
+        response = requests.get(url)
+        
+        # Extract and Parse the HTML content of the page if request successful
+        events_list = []
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Iterate through each row in table (event represented by a row)
+            event_items = soup.find_all('tr')
+            for event_item in event_items:
+                # Ensure table row is an event (otherwise operations will return none types)
+                if (event_item.get('itemtype') == 'http://schema.org/Event'):                
+                    event_name = event_item.find('a', class_='text-primary')
+                    sport = event_item.find('a', {'href': lambda x : x.startswith('/Sports/')})
+                    country = event_item.find('meta', itemprop='addressCountry')
+                    locality = event_item.find('meta', itemprop='addressLocality')
+                    date = event_item.find_all('td')[-1]
+                    
+                    # Create JSON objects with scraped data and set default values in case attribute cant be found
+                    events_list.append({
+                        'event_name': event_name.get_text() if event_name else None,
+                        'country': country.get('content') if country else None,
+                        'locality': locality.get('content') if locality else None,
+                        'date': date.get_text().strip() if date else None,
+                        'sport': sport.get_text() if sport else None
+                    })
+            yield json.dumps(events_list).encode('utf-8')
+        else:
+            yield json.dumps(None).encode('utf-8')
+
+    
     # Submit each call to a thread pool to speed up execution time
     with ThreadPoolExecutor() as executor:
         if weeks == 0: # Base case for unspecified amount of weeks (default to 1 week, the current one)
@@ -95,37 +96,15 @@ def process_request():
                 print("error")
             yield from future.result()
 
+
     return f"Get {weeks} weeks"
-'''
 
-
-@api.route("/events", methods=['POST'])
-def receive_query():
-    if request.method == 'POST':
-        query_list = request.get_json()
-        
-        for query in query_list:
-            yield query
-
-
-@api.route("/receive", methods=['POST'])
-def receive_request():
-    if request.method == 'POST':
-        data = request.get_json()  # Get the JSON data sent in the request
-        if 'key' in data:
-            value = data['key']  # Access the value associated with the 'key' key
-            # Process the received data here
-            return data
-        else:
-            return 'Key "key" not found in the received data', 400  # Return a 400 Bad Request error if 'key' is not found
-
-    
+ 
 
 if __name__ == "__main__":
-    # Specify port range to scan 
-    # API services will run from ports 5000 to 5004 when on the same machine (max 3 services on one machine)
+    # Specify port range to scan
     start_port = 5000
-    end_port = 5002
+    end_port = 5005
     address = "localhost"
     
     # Scan for open port to bind application to
@@ -136,6 +115,6 @@ if __name__ == "__main__":
         print("No open ports found in the specified range.")
         exit(0)
     else:
-        api.run(debug=True, port=port)
+        app.run(debug=True, port=port)
     
         
--- Routing Server/Router.py
+++ Routing Server/Router.py
@@ -1,51 +1,11 @@
-import requests 
-import socket
+import requests
 
 from flask import Flask
 
 
-def find_open_port(address, start_port, end_port):
-    """
-    Scans a range of ports for an available port to bind the given address to.
-    
-    Parameters:
-        address (string): The desired url of the service.
-        start_port (int): The smallest port to bind the service to.
-        end_port (int): The largest port to bind the service to.
-    """
-    for port in range(start_port, end_port + 1):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.bind((address, port))
-                return port
-            except OSError:
-                pass
-    return None
+app = Flask(__name__)
 
 
-router = Flask(__name__)
-
-
-@router.route('/')
-def send_request():
-    data_to_send = {'weeks': '5', 'country' : 'Canada', 'sport' : 'Soccer'}
-    response = requests.post('http://127.0.0.1:5000/events', json=data_to_send)
-    return response.text
-
-
-if __name__ == '__main__':
-    # Specify port range to scan
-    # Router services will run from ports 5003 to 5005 when on the same machine (max 3 services on one machine)
-    start_port = 5003
-    end_port = 5005
-    address = "localhost"
-    
-    # Scan for open port to bind application to
-    port = find_open_port(address, start_port, end_port)
-    
-    # Bind application if port is available
-    if port is None:
-        print("No open ports found in the specified range.")
-        exit(0)
-    else:
-        router.run(debug=True, port=port)
+@app.route('/')
+def Route():
+    print("Temp")
