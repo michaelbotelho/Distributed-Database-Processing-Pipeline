9a89f16542e6cb512a3e9c37c9fbb1609311a433
Michael Botelho <michaelbotelho51@gmail.com>
2024-04-09 02:20:36 -0400

    Multiple Changes
    - Merged scraping service and routing service into one file
    - Installed Redis local folder to attempt to programmatically start and stop the server.
    - Committing because im going to try and change the folder structure

--- API Server/CollectData.py
+++ API Server/CollectData.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import socket
 
 from flask import Flask, request, jsonify
--- Routing Server/Router.py
+++ Routing Server/Router.py
@@ -1,12 +1,17 @@
 import socket 
 import requests
-from flask import Flask, jsonify 
+import subprocess
+import time
+import redis
+from flask import Flask, request, jsonify 
+from bs4 import BeautifulSoup
 
 
-router = Flask(__name__)
+app = Flask(__name__)
+HOST_ADDRESS = 'localhost'
 
 
-def find_open_port(address, start_port, end_port):
+def find_open_port(address, start_port, end_port=65534):
     """
     Scans a range of ports for an available port to bind the given address to.
     
@@ -20,6 +25,7 @@ def find_open_port(address, start_port, end_port):
             try:
                 s.bind((address, port))
                 return port
+                break
             except OSError:
                 pass
     return None
@@ -31,38 +37,107 @@ def check_status_of_endpoint(address, port):
             return True 
     except OSError: 
         return False 
+ 
+    # Start the Redis server as a subprocess
+    subprocess.Popen('redis-cli shutdown', shell=True)
+    # Give some time for the server to start up
+    time.sleep(1)   
     
-
-# Relay query to API service 
-@router.route('/query') 
-def receive_query(): 
-    weeks = 3 
-    country = "Canada" 
-    sport = "Soccer" 
+def scrape_sports_events(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
     
-    # Search for active server on port range (defined in API service) 
-    for port in range(5000, 5002 + 1): 
-        api_url = f'http://localhost:{port}/' 
-        if check_status_of_endpoint('localhost', port): 
-            # Make a request to API service 
-            response = requests.get(api_url, params={'weeks': weeks, 'country': country, 'sport': sport}) 
-            return response.json() 
-        return jsonify(None) 
+    # Extract and Parse the HTML content of the page if request successful
+    events_list = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Iterate through each row in table (event represented by a row)
+        event_items = soup.find_all('tr')
+        for event_item in event_items:
+            # Ensure table row is an event (otherwise operations will return none types)
+            if (event_item.get('itemtype') == 'http://schema.org/Event'):                
+                event_name = event_item.find('a', class_='text-primary')
+                sport = event_item.find('a', {'href': lambda x : x.startswith('/Sports/')})
+                country = event_item.find('meta', itemprop='addressCountry')
+                locality = event_item.find('meta', itemprop='addressLocality')
+                date = event_item.find_all('td')[-1]
+                
+                # Create JSON objects with scraped data and set default values in case attribute cant be found
+                events_list.append({
+                    'event_name': event_name.get_text() if event_name else None,
+                    'country': country.get('content') if country else None,
+                    'locality': locality.get('content') if locality else None,
+                    'date': date.get_text().strip() if date else None,
+                    'sport': sport.get_text() if sport else None
+                })
+        return events_list
+    else:
+        return None
 
+    
+'''Application Routes'''
+# Receive a query
+@app.route('/', methods=['GET'])
+def receive_query():
+    # Simulate client get request
+    with app.test_request_context('/?weeks=2&country=Mexico&sport=Hockey'):
+        weeks = request.args.get('weeks')
+        country = request.args.get('country')
+        sport = request.args.get('sport')
+        
+        query = request.query_string
+        
+    
+        
+    # Make a request to API service
+    scraping_service_url = f'http://{HOST_ADDRESS}:{APPLICATION_PORT-1}/scrape'
+    response = requests.get(scraping_service_url, params={'weeks': weeks})
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        try:
+            # Cache query and response
+            #redis_client.hset(query.decode('utf-8'), 'events', response.json())
+            
+            # Send response back to client
+            # return jsonify(response.json)
+            return jsonify({'query': query.decode('utf-8'), 'data' : response.json()})
+        except ValueError as e:
+            print(f"Error decoding JSON: {e}")
+            return jsonify({'error': 'Invalid JSON format in response'})
+    else:
+        print(f"Error: {response.status_code}")
+        return jsonify({'error': 'Failed to fetch data from API service'})
+
+
+# Collect data in range of weeks (default weeks=0)
+@app.route("/scrape", methods=['GET'])
+def collect_data():
+    if request.method == 'GET':
+        weeks = request.args.get('weeks')
+
+        response = [] 
+        if not weeks or int(weeks) == 0:
+            response_data = scrape_sports_events(url=f'https://allsportdb.com/?week={0}')
+            if response_data:
+                response.append(response_data)
+        else:
+            for week in range(int(weeks)):
+                response_data = scrape_sports_events(url=f'https://allsportdb.com/?week={week}')
+                if response_data:
+                    response.append(response_data)
+
+        return jsonify(response)
+    
 
 if __name__ == '__main__': 
-    # Specify port range to scan
-    # Router services will run from ports 5003 to 5005 when on the same machine (max 3 services on one machine) 
-    start_port = 5003 
-    end_port = 5005 
-    address = "localhost" 
-    
     # Scan for open port to bind application to 
-    port = find_open_port(address, start_port, end_port) 
-    
+    APPLICATION_PORT = find_open_port(HOST_ADDRESS, start_port=5000, end_port=5004)
+    # Scan for open port to bind redis client to
+    REDIS_PORT = find_open_port(HOST_ADDRESS, start_port=6379)
     # Bind application if port is available 
-    if port is None: 
+    if APPLICATION_PORT is None: 
         print("No open ports found in the specified range.") 
         exit(0) 
     else: 
-        router.run(debug=True, port=port)
+        app.run(debug=True, port=APPLICATION_PORT) # Weird bug where application binds to PORT and print returns PORT + 1
